8df71c356cba3deda46603a46bd14c3568b8d191
LuciferDarkonster <trantrungbaokha@gmail.com>
2023-06-04 02:25:36 +0700

    trang thống kê doanh thu theo tháng

--- web_banvemaybay/Areas/Admin/Controllers/thongkeController.cs
+++ web_banvemaybay/Areas/Admin/Controllers/thongkeController.cs
@@ -11,11 +11,13 @@ namespace web_banvemaybay.Areas.Admin.Controllers
     {
 
         // GET: Admin/thongke
-            public ActionResult Index(DateTime monthYear)
+            public ActionResult Index(String monthYear)
             {
+                DateTime monthy = new DateTime(int.Parse(monthYear), 1,1);
                 web_banvemaybayEntities db = new web_banvemaybayEntities();
-                DateTime startDate = new DateTime(monthYear.Year, 1, 1);
-                DateTime endDate = new DateTime(monthYear.Year, 12, 31);
+                if (monthy == null) { monthy = DateTime.Now; }
+                DateTime startDate = new DateTime(monthy.Year, 1, 1);
+                DateTime endDate = new DateTime(monthy.Year, 12, 31);
                 var ves = db.Ve.Where(c => c.Ngaydatve >= startDate && c.Ngaydatve <= endDate).ToList();
                 var tongDanhThuThang = new List<Doanhthu>();
 
@@ -26,14 +28,40 @@ namespace web_banvemaybay.Areas.Admin.Controllers
 
                     var doanhThuThang = new Doanhthu
                     {
-                        thang = $"{month}/{monthYear.Year}",
+                        thang = $"{month}",
                         doanhthu = tongTienThang
                     };
 
                     tongDanhThuThang.Add(doanhThuThang);
                 }
-
-                return View(tongDanhThuThang);
+            Session["NamDuocChon"] = monthy.Year.ToString();
+            return View(tongDanhThuThang);
             }
+        public ActionResult DoanhThuNamNay()
+        {
+            web_banvemaybayEntities db = new web_banvemaybayEntities();
+            DateTime monthYear = DateTime.Now;
+            DateTime startDate = new DateTime(monthYear.Year, 1, 1);
+            DateTime endDate = new DateTime(monthYear.Year, 12, 31);
+            var ves = db.Ve.Where(c => c.Ngaydatve >= startDate && c.Ngaydatve <= endDate).ToList();
+            var tongDanhThuThang = new List<Doanhthu>();
+
+            for (int month = 1; month <= 12; month++)
+            {
+                var vesThang = ves.Where(c => c.Ngaydatve.Month == month);
+                double tongTienThang = vesThang.Sum(c => c.Gia);
+
+                var doanhThuThang = new Doanhthu
+                {
+                    //thang = $"{month}/{monthYear.Year}",
+                    thang = $"{month}",
+                    doanhthu = tongTienThang
+                };
+
+                tongDanhThuThang.Add(doanhThuThang);
+            }
+            Session["NamDuocChon"] = monthYear.Year.ToString();
+            return View(tongDanhThuThang);
+        }
     }
 }
