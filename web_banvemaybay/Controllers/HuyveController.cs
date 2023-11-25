56635b060c8443bea4f21a4eccea6473e5a15101
nguyenhuytam <85795981+nguyenhuytam@users.noreply.github.com>
2023-11-25 23:57:49 +0700

    check ve

--- web_banvemaybay/Controllers/HuyveController.cs
+++ web_banvemaybay/Controllers/HuyveController.cs
@@ -18,6 +18,7 @@ namespace web_banvemaybay.Controllers
             int mave = int.Parse(@Session["mavemuontra"].ToString());
             ViewBag.id = mave;
             var checkma = db.Ve.Where(c => c.IDve.Equals(mave)).FirstOrDefault();
+            var checkmafrom = db.Ve.Where(c => c.IDve.Equals(mave+1)).FirstOrDefault();
             ViewBag.Tenkhach = checkma.TTlienhe.FullName;
             ViewBag.NgayBay = checkma.Chuyenbay.Ngaydi.Value;
             ViewBag.dddi = checkma.Chuyenbay.Diadiemdi;
@@ -36,6 +37,7 @@ namespace web_banvemaybay.Controllers
                     double tienphat = checkma.Gia - tienhoang;
                     string message = "Yêu Cầu Hủy Vé";
                     checkma.Tinhtrang = message;
+                    checkma.Tienphat = tienphat;
                     db.Ve.AddOrUpdate(checkma);
                     db.SaveChanges();
                     string thongbao = "Số tiền hoàng sẽ được hoàng trả trong vòng 7 ngày vui vòng liên hệ quầy bán vé gần nhất để thực hiện giải quyết";
@@ -50,10 +52,6 @@ namespace web_banvemaybay.Controllers
                     }
                     db.Chuyenbay.AddOrUpdate(chuyenbay);
                     db.SaveChanges();
-                    checkma.Tienphat = tienphat;
-                    ViewBag.TienHoang = tienhoang;
-                    ViewBag.tienphat = tienphat;
-                    ViewBag.thongbao = thongbao;
                 }
                 else
                 {
@@ -68,8 +66,8 @@ namespace web_banvemaybay.Controllers
                 ViewBag.Message = message;
                 checkma.Tinhtrang = message;
                 db.Ve.AddOrUpdate(checkma);
-               db.SaveChanges();
-               var chuyenbay = db.Chuyenbay.Where(c=> c.IDchuyenbay == checkma.IDchuyenbay).FirstOrDefault();
+               db.SaveChanges();  
+                var chuyenbay = db.Chuyenbay.Where(c=> c.IDchuyenbay == checkma.IDchuyenbay).FirstOrDefault();
                 if (checkma.Hangve.IDhangve == 1)
                 {
                     chuyenbay.ThuongGia += 1 * checkma.Sove;
@@ -79,9 +77,77 @@ namespace web_banvemaybay.Controllers
                     chuyenbay.PhoThong += 1 * checkma.Sove;
                 }
                 db.Chuyenbay.AddOrUpdate(chuyenbay);
-                db.SaveChanges();
+                db.SaveChanges();             
             }
+
+            if(checkma.IDchuyenbayve != null)
+            {
+                Session["mavefrom"] = mave + 1;
+                ViewBag.idfrom = mave + 1;
+                ViewBag.Tenkhachfrom = checkmafrom.TTlienhe.FullName;
+                ViewBag.NgayBayfrom = checkmafrom.Chuyenbay.Ngaydi.Value;
+                ViewBag.dddifrom = checkmafrom.Chuyenbay.Diadiemdi;
+                ViewBag.dddenfrom = checkmafrom.Chuyenbay.Diadiemden;
+                ViewBag.hangfrom = checkmafrom.Chuyenbay.HangHK.TenHang;
+                ViewBag.macbfrom = checkmafrom.Chuyenbay.IDchuyenbay;
+                if (checkmafrom.Tinhtrang == "Đã thanh toán")
+                {
+                    DateTime ngayhienhtai = DateTime.Today;
+                    DateTime ngayxuatphat = checkmafrom.Chuyenbay.Ngaydi.Value;
+                    int sosanh = (ngayxuatphat - ngayhienhtai).Days;
+                    if (sosanh >= 3)
+                    {
+                        double tienhoang;
+                        tienhoang = checkmafrom.Gia - ((30 * checkmafrom.Gia) / 100);
+                        double tienphat = checkmafrom.Gia - tienhoang;
+                        string message = "Yêu Cầu Hủy Vé";
+                        checkmafrom.Tinhtrang = message;
+                        checkmafrom.Tienphat = tienphat;
+                        db.Ve.AddOrUpdate(checkmafrom);
+                        db.SaveChanges();
+                        string thongbao = "Số tiền hoàng sẽ được hoàng trả trong vòng 7 ngày vui vòng liên hệ quầy bán vé gần nhất để thực hiện giải quyết";
+                        var chuyenbay = db.Chuyenbay.Where(c => c.IDchuyenbay == checkmafrom.IDchuyenbay).FirstOrDefault();
+                        if (checkmafrom.Hangve.IDhangve == 1)
+                        {
+                            chuyenbay.ThuongGia += 1 * checkmafrom.Sove;
+                        }
+                        else
+                        {
+                            chuyenbay.PhoThong += 1 * checkmafrom.Sove;
+                        }
+                        db.Chuyenbay.AddOrUpdate(chuyenbay);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        string thongbao = "Xin lỗi bạn không thể thực hiện hủy vé trong thời gian xác ngày xuất phát !!! Chỉ được thực hiện hủy vé cách thời gian xuất phát là 3 ngày";
+                        ViewBag.thongbao = thongbao;
+                    }
+
+                }
+                else
+                {
+                    string message = "Đã hủy ";
+                    ViewBag.Message = message;
+                    checkmafrom.Tinhtrang = message;
+                    db.Ve.AddOrUpdate(checkmafrom);
+                    db.SaveChanges();
+                    var chuyenbay = db.Chuyenbay.Where(c => c.IDchuyenbay == checkmafrom.IDchuyenbay).FirstOrDefault();
+                    if (checkmafrom.Hangve.IDhangve == 1)
+                    {
+                        chuyenbay.ThuongGia += 1 * checkmafrom.Sove;
+                    }
+                    else
+                    {
+                        chuyenbay.PhoThong += 1 * checkmafrom.Sove;
+                    }
+                    db.Chuyenbay.AddOrUpdate(chuyenbay);
+                    db.SaveChanges();
+                }
+            }    
+
             
+
             return View();
         }
     }
